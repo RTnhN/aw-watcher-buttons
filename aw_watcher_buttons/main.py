52d3500bbfef6f8aca064568a644ef93c9a699fa
RTnhN <43109787+RTnhN@users.noreply.github.com>
2024-08-25 14:36:40 -0500

    Change polling time
    Activities were not merged correctly, so this fixes that.

--- aw_watcher_buttons/main.py
+++ aw_watcher_buttons/main.py
@@ -73,13 +73,13 @@ def main():
                 data = {"title": title, "button": button_name}
                 print_statusline(title)
                 event = Event(timestamp=datetime.now(timezone.utc), data=data)
-                aw.heartbeat(bucketname, event, pulsetime=poll_time + 1, queued=True)
+                aw.heartbeat(bucketname, event, pulsetime=poll_time + 5, queued=True)
             else:
                 title = "No button pressed"
                 data = {"title": title, "button": "none"}
                 print_statusline(title)
                 event = Event(timestamp=datetime.now(timezone.utc), data=data)
-                aw.heartbeat(bucketname, event, pulsetime=poll_time + 1, queued=True)
+                aw.heartbeat(bucketname, event, pulsetime=poll_time + 5, queued=True)
             current_time = datetime.now()
             if state != -1:
                 if current_time.minute % 15 == 0 and current_time.second == 0:
